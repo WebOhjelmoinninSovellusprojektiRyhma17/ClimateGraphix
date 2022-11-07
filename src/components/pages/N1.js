84fb2a6c3d3bc846ad5af0a1a9f7cad490e0a727
EriKorh <t1koer00@students.oamk.fi>
2022-11-07 16:48:19 +0200

    Rikki

---
+++ src/components/pages/N1.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function N1() {
+    return (
+        <p>N1 page</p>
+    )
+}
---
+++ src/components/pages/N2.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function N2() {
+    return (
+        <p>N2 page</p>
+    )
+}
