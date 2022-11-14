c3fcb35de31553e633ef3366c67645afa4ba7179
Ruandalainen <johannessdkortti@gmail.com>
2022-11-14 11:23:01 +0200

    Login sivu aloitettu

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,6 +1,22 @@
 import NavBar from "../NavBar";
 
+const [userB, getUserB] = useState([]);
+
+const URL = 'http://localhost:3001/'
+
 export default function Login() {
+
+    const getUserdata = () => {
+        axios.get(`${URL}users`) 
+            .then((response) => {
+                getUserB(response.data);
+            }).catch(error =>
+                console.error(`Error: ${error}`));
+    }
+
+    
+
+
     return (
         <>
             <NavBar />
