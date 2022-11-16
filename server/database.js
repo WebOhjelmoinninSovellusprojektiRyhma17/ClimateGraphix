4628be519e6d7a0dffce2df321af443f5c2600af
Hietanne <hietanne@gmail.com>
2022-11-16 14:26:59 +0200

    Yhdistetty pilveen

--- server/database.js
+++ server/database.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 const connection = mysql.createPool({
-  host: '127.0.0.1',
+  host: '34.88.182.164',
   user: 'root',
-  password: 'root',
+  password: 'h+AIbhiKX8Of0dO',
   database: 'mydb'
 });
 module.exports = connection;
