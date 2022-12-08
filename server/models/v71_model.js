b2f89fd4d18e919145de309359fdc9af466451e1
EriKorh <t1koer00@students.oamk.fi>
2022-12-08 18:11:05 +0200

    Pieni pilkkuvirhe

--- server/models/v71_model.js
+++ server/models/v71_model.js
@@ -2,7 +2,7 @@ const db = require('../database');
 
 const v71 = {
     getAll: function(callback) {
-      return db.query('select * from v71`', callback);
+      return db.query('select * from v71', callback);
     }
   };
   module.exports = v71;
